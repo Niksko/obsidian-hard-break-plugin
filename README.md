51625bc98191b54565123c3502c039f146817e97
Nik Skoufis <nskoufis@seek.com.au>
2023-07-23 19:43:59 +1000

    Implement a bit of basic functionality
    This implements some of the basic functionality as described in the
    README.
    As noted in the README, there's still a small bug where undoing a
    linebreak won't quite work as you expect if you've only just broken the
    line and not made it much further.

--- README.md
+++ README.md
@@ -1,17 +1,45 @@
 # Obsidian hard break plugin
 
 This plugin allows Obsidian to insert hard linebreaks in long lines, emulating a feature that I like in Webstorm.
-This allows markdown to remain readable, and makes it easier to navigate markdown documents because they have 
+This allows markdown to remain readable, and makes it easier to navigate markdown documents because they have
 shorter lines.
 
-This is a bit of a toy to help explore how to write Obsidian plugins.
+This is a bit of a toy to help explore how to write Obsidian plugins, but it also fixes a genuine frustration.
 
-Still very much TODO, but so far I have the core functionality working where if you take a line over 80 characters, 
-it will remove the current word, add a newline, and the put the word on the new line.
+What's working:
 
-Still TODO (these also have comments in the code):
-* Refactor so that we're not computing whether the word is too long on every change in an update
-* Add some settings, at the very least a configurable length to break on
-* Webstorm's implementation is fairly clever, it will continue with the current markdown block.
-  For example, if I'm in the middle of typing a comment and I hit the length limit, it will break but it will assume 
-  I want to continue writing the comment, so it will add the right amount of indentation to do this.
+* Lines will break at a specified line length
+* This length is configurable via plugin settings
+* If you're in the middle of a word, it will be moved to the next line
+* Preceding whitespace will be trimmed after the word you're in the middle of is moved to the next line
+* If you break the line yourself at the exact length of the line, the plugin will not add a second newline, as you 
+  would expect
+* If you're in the middle of whitespace when the line limit is reached, no whitespace will be trimmed
+
+What still needs work (in order of priority):
+
+* If you want to override the line break by going to the start of the newly broken line and backspacing, this won't 
+  work if you immediately backspace.
+  This is because since we trimmed the whitespace, your line length is now back at the threshold for breaking.
+  I _think_ to solve this we might need to use some state to temporarily disable checking for longer lines until 
+  either we exceed the line length, or we move our selection somewhere else.
+* Pretty sure this will not work for right-to-left texts.
+  I need to adjust all of the manual offsetting I'm doing to be inverted for RTL text
+
+## Development
+
+Watch for changes and continuously rebuild: `pnpm run dev`
+
+Lint and check formatting: `pnpm run lint`
+
+Prettier format: `pnpm run format`
+
+## Release process
+
+1. If the new code requires a newer version of Obsidian, update `minAppVersion` in `manifest.json`
+1. Run `pnpm version patch|minor|major` to bump the versions in the `manifest.json` and the `package.json` and update 
+   the `versions.json` file
+1. Build plugin code and CSS: `pnpm run build`
+1. Create a GitHub release with the new version number, without the `v` in front
+1. Upload the `main.js`, `styles.css` and `manifest.json` to the release
+1. Make a PR to [https://github.com/obsidianmd/obsidian-releases]()
--- main.ts
+++ main.ts
@@ -1,145 +1,172 @@
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
-import { Extension, Facet, Text } from "@codemirror/state";
+	CharCategory,
+	EditorSelection,
+	EditorState,
+	SelectionRange,
+	TransactionSpec,
+} from "@codemirror/state";
 import { EditorView, ViewUpdate } from "@codemirror/view";
 
-// TODO: Add some settings here for what line length to break on
 interface HardBreakPluginSettings {
-	mySetting: string;
+	hardWrapLineLength: number;
 }
 
 const DEFAULT_SETTINGS: HardBreakPluginSettings = {
-	mySetting: "default",
+	hardWrapLineLength: 50,
 };
 
-const isLongLineChange = (view: EditorView) => {
-	return (
-		fromA: number,
-		toA: number,
-		fromB: number,
-		toB: number,
-		inserted: Text,
-	) => {
-		const state = view.state;
-		if (state.doc.lineAt(toB).length > 80) {
-			// TODO: currently doesn't handle backspacing to override the short line length. The backspace just
-			//  immediately triggers this again. Might need to watch the changes to see how to detect this and not
-			//  add the newline.
-			const wordSelectionAtEndOfLine = state.wordAt(toB);
-			if (wordSelectionAtEndOfLine?.empty == false) {
-				const wordFrom = wordSelectionAtEndOfLine.from;
-				const wordTo = wordSelectionAtEndOfLine.to;
-				const wordAtEndOfLine = state.sliceDoc(wordFrom, wordTo);
-				const changes = [
-					{
-						from: wordFrom,
-						to: wordTo,
-					},
-					{
-						from: wordFrom,
-						to: wordFrom,
-						insert: state.lineBreak,
-					},
-					{
-						from: wordFrom + 1,
-						to: wordFrom + 1,
-						insert: wordAtEndOfLine,
-					},
-				];
-				view.dispatch({
-					changes,
-					selection: {
-						anchor: wordFrom + 1 + wordAtEndOfLine.length,
-					},
-				});
-			}
-		}
-	};
+const shouldHardWrap = (
+	oldState: EditorState,
+	newState: EditorState,
+	hardWrapLength: number,
+): boolean => {
+	const newSelection = newState.selection;
+	const newSelectionHead = newSelection.ranges[newSelection.mainIndex].head;
+	const newSelectionLine = newState.doc.lineAt(newSelectionHead);
+	// On creating a new line (either as part of this plugin, or through other actions that create a new line)
+	// the old state doesn't include the new line. We don't need to do anything in this case, so return false
+	if (oldState.doc.lines < newSelectionLine.number) {
+		return false;
+	}
+	const oldSelection = oldState.selection;
+	const oldSelectionLine = oldState.doc.lineAt(
+		oldSelection.ranges[oldSelection.mainIndex].head,
+	);
+	// If the old selection line is greater than the new selection line, we're backspacing and removing a break
+	// Also do nothing, because we want you to be able to manually un-break an auto-broken line
+	if (oldSelectionLine.number > newSelectionLine.number) {
+		return false;
+	}
+	const newLineLength = newSelectionLine.length;
+	const oldLineLength = oldState.doc.line(newSelectionLine.number).length;
+	return oldLineLength <= hardWrapLength && newLineLength > hardWrapLength;
 };
 
-const editorExtension: Extension = [
-	EditorView.updateListener.of((update: ViewUpdate) => {
-		if (update.docChanged) {
-			// TODO: Needs a refactor, this probably doesn't need to iterate over all changes, perhaps it just wants to
-			//  look at one of the changes? Or compare the old and the new state to see whether it pushed a line
-			//  length over 80 characters?
-			update.changes.iterChanges(isLongLineChange(update.view));
+const findWhitespaceBefore = (
+	state: EditorState,
+	pos: number,
+): SelectionRange => {
+	let foundWhitespace = true;
+	let leftmostWhitespace = pos;
+	while (foundWhitespace) {
+		const currentChar = state.doc.slice(
+			leftmostWhitespace,
+			leftmostWhitespace + 1,
+		);
+		if (
+			state.charCategorizer(leftmostWhitespace)(currentChar.toString()) ==
+			CharCategory.Space
+		) {
+			leftmostWhitespace = leftmostWhitespace - 1;
+		} else {
+			foundWhitespace = false;
 		}
-	}),
-];
+	}
+	return EditorSelection.range(leftmostWhitespace - 1, pos);
+};
 
-export default class MyPlugin extends Plugin {
+const createWrapLineTransaction = (
+	state: EditorState,
+): TransactionSpec => {
+	const newSelectionHead =
+		state.selection.ranges[state.selection.mainIndex].head;
+	const wordSelectionAtEndOfLine = state.wordAt(newSelectionHead);
+	if (wordSelectionAtEndOfLine?.empty == false) {
+		// If there's a word at the end of the line, we need to:
+		// * Remove the word at the end of the line
+		// * Remove any preceding whitespace
+		// * Insert a line break
+		// * Add the removed word on the new line (after the line break)
+		const wordFrom = wordSelectionAtEndOfLine.from;
+		const wordTo = wordSelectionAtEndOfLine.to;
+		const wordAtEndOfLine = state.sliceDoc(wordFrom, wordTo);
+		const precedingWhitespace = findWhitespaceBefore(state, wordFrom);
+		const changes = [
+			{
+				from: wordFrom,
+				to: wordTo,
+			},
+			{
+				from: precedingWhitespace.anchor,
+				to: wordTo,
+			},
+			{
+				from: precedingWhitespace.anchor,
+				to: precedingWhitespace.anchor,
+				insert: state.lineBreak,
+			},
+			{
+				from: precedingWhitespace.anchor + 1,
+				to: precedingWhitespace.anchor + 1,
+				insert: wordAtEndOfLine,
+			},
+		];
+		return {
+			changes,
+			selection: {
+				anchor: precedingWhitespace.anchor + 1 + wordAtEndOfLine.length,
+			},
+		};
+	} else {
+		// There's no word at the end of the line. If the character that took us over the line length limit was a
+		// newline, then we have nothing to do
+		const finalLineCharacter = state.sliceDoc(
+			newSelectionHead,
+			newSelectionHead,
+		);
+		if (finalLineCharacter == "\n") {
+			return {};
+		}
+		// Otherwise, delete the whitespace that was just added and add a newline instead
+		const changes = [
+			{
+				from: newSelectionHead - 1,
+				to: newSelectionHead,
+			},
+			{
+				from: newSelectionHead - 1,
+				to: newSelectionHead - 1,
+				insert: state.lineBreak,
+			},
+		];
+		return {
+			changes,
+			selection: {
+				anchor: newSelectionHead,
+			},
+		};
+	}
+};
+
+export default class HardBreakPlugin extends Plugin {
 	settings: HardBreakPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
-			},
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new HardBreakSettingsTab(this.app, this));
 
-		this.registerEditorExtension(editorExtension);
+		this.registerEditorExtension(
+			EditorView.updateListener.of((update: ViewUpdate) => {
+				if (
+					update.docChanged &&
+					shouldHardWrap(
+						update.startState,
+						update.state,
+						// If we wrap when the line is at hardWrapLineLength, when we add the newline, it will be 1
+						// over the limit. So we have to wrap at 1 less than the limit.
+						this.settings.hardWrapLineLength - 1,
+					)
+				) {
+					const hardWrapTransaction = createWrapLineTransaction(
+						update.state,
+					);
+					update.view.dispatch(hardWrapTransaction);
+				}
+			}),
+		);
 	}
 
 	onunload() {}
@@ -157,26 +184,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class HardBreakSettingsTab extends PluginSettingTab {
+	plugin: HardBreakPlugin;
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HardBreakPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -187,16 +198,17 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
+			.setName("Hard break length")
+			.setDesc("The line length to hard break a line at")
+			.addSlider((slider) => {
+				slider
+					.setLimits(50, 240, 1)
+					.setValue(this.plugin.settings.hardWrapLineLength)
+					.setDynamicTooltip()
+					.onChange(async (value: number) => {
+						this.plugin.settings.hardWrapLineLength = value;
 						await this.plugin.saveSettings();
-					}),
-			);
+					});
+			});
 	}
 }
